010ffe273b11552716746fd9063c62aa2a680da4
kamScripts <k.gebski1@gmail.com>
2025-11-06 11:35:14 +0000

    max_len functionality

--- data_structures/arrayDequeue.py
+++ data_structures/arrayDequeue.py
@@ -2,8 +2,9 @@ class Empty(Exception):
     """Empty deque exception"""
 class Arraydeque:
     """ Deque Abstract Data Type (ADT)."""
-    DEFAULT_CAPACITY=5 #moderate capacity for all new queues
-    def __init__(self)->None:
+    DEFAULT_CAPACITY=10 #moderate capacity for all new queues
+    def __init__(self, max_len=None)->None:
+        self.max_len = max_len
         self._data=[None]*Arraydeque.DEFAULT_CAPACITY
         self._size=0
         self._front=0
@@ -27,20 +28,37 @@ class Arraydeque:
         return ','.join(result)
 
     def add_first(self,e)->None:
-        """Add element to the front of deque."""
-        if self._size == len(self._data):
-            self._resize(2 * len(self._data))
-        self._front = (self._front - 1) % len(self._data)
-        self._data[self._front] = e
-        self._size+=1
+        """Add element to the front of deque,
+        if max_len set, last element is pushed out when reach
+        capacity limit."""
+        # if reach max len remove pop out element from the other end
+        if self.max_len and self._size == self.max_len:
+            self.delete_last()
+            self._front = (self._front - 1) % len(self._data)
+            self._data[self._front] = e
+            self._size+=1
+        else:
+            if self._size == len(self._data):
+                self._resize(2 * len(self._data))
+            self._front = (self._front - 1) % len(self._data)
+            self._data[self._front] = e
+            self._size+=1
 
     def add_last(self,e)->None:
-        """Add element e to the back of a deque."""
-        if self._size==len(self._data):
-            self._resize(2*len(self._data))
-        avail=(self._front+self._size)%len(self._data)
-        self._data[avail]=e
-        self._size+=1
+        """Add element e to the back of a deque,
+        if max_len is on element at the front of deque is pushed out
+        when reach capacity limit"""
+        if self.max_len and self._size == self.max_len:
+            self.delete_first()
+            avail=(self._front+self._size)%len(self._data)
+            self._data[avail]=e
+            self._size+=1
+        else:
+            if self._size==len(self._data):
+                self._resize(2*len(self._data))
+            avail=(self._front+self._size)%len(self._data)
+            self._data[avail]=e
+            self._size+=1
 
     def delete_first(self):
         """Remove and return first element from deque"""
@@ -94,29 +112,30 @@ class Arraydeque:
         self._front = 0
 
 if __name__=="__main__":
-    d=Arraydeque()
-    d.add_last(5)
-    d.add_first(3)
-    d.add_first(7)
+    d=Arraydeque(10)
+    #d.add_last(5)
+    #d.add_first(3)
+    #d.add_first(7)
+    #print(d)
+    #print('first',d.first(), sep=' -> ')
+    #print('delete last', d.delete_last(), sep=' -> ')
+    #print('len(d)', len(d), sep=' -> ')
+    #print('delete last', d.delete_last(), sep=' -> ')
+    #print('delete last', d.delete_last(), sep=' -> ')
+    #d.add_first(6)
+    #print('last', d.last(), sep=' -> ')
+    #d.add_first(8)
+    #d.add_first(1)
+    #d.add_first(62)
+    #print(d.is_empty())
+    #print('last', d.last(), sep=' -> ')
+    #print(d)
+    #print('delete first', d.delete_first(), sep=' -> ')
+    #d.add_last(89)
+    #print(d)
+    for num in range(14):
+        d.add_first(num)
     print(d)
-    print('first',d.first(), sep=' -> ')
-    print('delete last', d.delete_last(), sep=' -> ')
-    print('len(d)', len(d), sep=' -> ')
-    print('delete last', d.delete_last(), sep=' -> ')
-    print('delete last', d.delete_last(), sep=' -> ')
-    d.add_first(6)
-    print('last', d.last(), sep=' -> ')
-    d.add_first(8)
-    d.add_first(1)
-    d.add_first(62)
-
-
-    print(d.is_empty())
-    print('last', d.last(), sep=' -> ')
+    for num in range(40,45):
+        d.add_last(num)
     print(d)
-    print('delete first', d.delete_first(), sep=' -> ')
-    d.add_last(89)
-
-    print(d)
-
-
--- data_structures/arrayStack.py
+++ data_structures/arrayStack.py
@@ -6,18 +6,29 @@ class ArrayStack:
     """Example from Goodrich et al. Data Structures & Algorithms(2013)
     LIFO Stack implementation using a Python list as underlying storage
     """
-
-    def __init__(self):
+    def __init__(self, max_len=None):
         """Create an empty stack."""
         self._data=[]
+        self._size=0
+        self._max_len=max_len
+        
     def __len__(self):
         """Return the number of elements in the stack."""
         return len(self._data)
+
+    def __str__(self):
+        
+        prnt = [str(item) for item in self._data]
+        prnt.reverse()
+        return f"Stack top -> bottom: \n\t{'\n\t'.join(prnt)}"
+    
     def is_empty(self):
         """Return True if the stack is empty."""
         return len(self._data)==0
     def push(self,e):
         """Add element e to the top of the stack."""
+        if self._max_len and len(self._data)==self._max_len:
+            raise Full('The stack raised maximum capacity')
         self._data.append(e)    #New item stored at the end of the list
     def top(self):
         """
@@ -36,15 +47,24 @@ class ArrayStack:
         return self._data.pop()
     def transfer(self,s,t):
         """Transfer Stack S into  empty T where S,last=t.top"""
-        for _ in range(len(S)):
+        for _ in range(len(s)):
             t.push(s.pop())
-        
-l=[1,2,3,4,5]
-S=ArrayStack()
-
-for num in l:
-    S.push(num)
-print(S.top(),S._data, sep=('-> '))
-T=ArrayStack()
-T.transfer(S,T)
-print(T.top(),T._data,sep='-> ')
+    def clear(self):
+        """Removes all elements from a stack"""
+        if not self._data:
+            return
+        self.pop()
+        self.clear()
+if __name__=='__main__':
+    l=[1,2,3,4,5,6,7,8,9,10]
+    S=ArrayStack()
+    
+    for num in l:
+        S.push(num)
+    print(S)
+    print('S top:',S.top())
+    T=ArrayStack()
+    T.transfer(S,T)
+    T.clear()
+    print(T)
+    T.top()
