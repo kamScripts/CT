abecc1c2051568e0b4b43b083aad53b6c65dbb3b
kamScripts <k.gebski1@gmail.com>
2025-11-06 09:32:23 +0000

    transfer stack

--- data_structures/arrayDequeue.py
+++ data_structures/arrayDequeue.py
@@ -1,22 +1,30 @@
 class Empty(Exception):
     """Empty deque exception"""
 class Arraydeque:
-    """The Deque Abstract Data Type (ADT)"""
+    """ Deque Abstract Data Type (ADT)."""
     DEFAULT_CAPACITY=5 #moderate capacity for all new queues
     def __init__(self)->None:
         self._data=[None]*Arraydeque.DEFAULT_CAPACITY
         self._size=0
         self._front=0
-        
+
     def __len__(self):
         """Return length of the deque."""
         return self._size
+
     def __str__(self):
         result = []
         for i in range(self._size):
             index = (self._front + i) % len(self._data)
             result.append(str(self._data[index]))
         return ' <- '.join(result)
+
+    def __repr__(self):
+        result = []
+        for i in range(self._size):
+            index = (self._front + i) % len(self._data)
+            result.append(str(self._data[index]))
+        return ','.join(result)
 
     def add_first(self,e)->None:
         """Add element to the front of deque."""
@@ -25,7 +33,7 @@ class Arraydeque:
         self._front = (self._front - 1) % len(self._data)
         self._data[self._front] = e
         self._size+=1
-        
+
     def add_last(self,e)->None:
         """Add element e to the back of a deque."""
         if self._size==len(self._data):
@@ -33,7 +41,7 @@ class Arraydeque:
         avail=(self._front+self._size)%len(self._data)
         self._data[avail]=e
         self._size+=1
-        
+
     def delete_first(self):
         """Remove and return first element from deque"""
         if self.is_empty():
@@ -43,7 +51,7 @@ class Arraydeque:
         self._front = (self._front + 1) % len(self._data)
         self._size-=1
         return answer
-        
+
     def delete_last(self):
         """Remove and return last element from deque"""
         if self.is_empty():
@@ -52,22 +60,25 @@ class Arraydeque:
         self._data[(self._front+self._size-1) % len(self._data)] = None
         self._size -= 1
         return answer
-        
+
     def first(self):
         """Return(but do not remove) the first el of deque
         raise exception if is empty."""
         if self.is_empty():
             raise Empty("deque is empty")
         return self._data[self._front]
+    
     def last(self):
         """Return  (but do not remove) the last element of deque;
         error occurs if deque is empty"""
         if self.is_empty():
             raise Empty('deque is empty')
         return self._data[(self._front+self._size-1) % len(self._data)]
+    
     def is_empty(self):
         """Return True if deque is empty."""
         return self._size==0
+    
     def _resize(self, cap:int)->None:
         """Resize a deque if reach max capacity"""
         old=self._data
@@ -77,31 +88,31 @@ class Arraydeque:
             self._data[k]=old[walk]
             walk=(1 + walk) % len(old)
         self._front = 0
-        
+
 if __name__=="__main__":
-    
     d=Arraydeque()
     d.add_last(5)
     d.add_first(3)
     d.add_first(7)
-    print('deque:',d._data,sep=' -> ')
     print(d)
     print('first',d.first(), sep=' -> ')
-#    print('delete last', d.delete_last(), sep=' -> ')
-#    print('len(d)', len(d), sep=' -> ')
-#    print('delete last', d.delete_last(), sep=' -> ')
-#    print('delete last', d.delete_last(), sep=' -> ')
-#    d.add_first(6)
-#    print('last', d.last(), sep=' -> ')
-#    d.add_first(8)
-#    d.add_first(1)
-#    d.add_first(62)
-#
-#
-#    print(d.is_empty())
-#    print('last', d.last(), sep=' -> ')
-#    print('deque:',d._data,sep=' -> ')
-#    print('delete first', d.delete_first(), sep=' -> ')
-#    print(d._front)
+    print('delete last', d.delete_last(), sep=' -> ')
+    print('len(d)', len(d), sep=' -> ')
+    print('delete last', d.delete_last(), sep=' -> ')
+    print('delete last', d.delete_last(), sep=' -> ')
+    d.add_first(6)
+    print('last', d.last(), sep=' -> ')
+    d.add_first(8)
+    d.add_first(1)
+    d.add_first(62)
+
+
+    print(d.is_empty())
+    print('last', d.last(), sep=' -> ')
+    print(d)
+    print('delete first', d.delete_first(), sep=' -> ')
+    d.add_last(89)
+
+    print(d)
 
 
--- data_structures/arrayStack.py
+++ data_structures/arrayStack.py
@@ -1,5 +1,7 @@
 class Empty(Exception):
-    """Error attempting to access an element from an empty container."""
+    """Exception attempting to access an element from an empty container."""
+class Full(Exception):
+    """Exception attempting to push into full stack."""
 class ArrayStack:
     """Example from Goodrich et al. Data Structures & Algorithms(2013)
     LIFO Stack implementation using a Python list as underlying storage
@@ -32,3 +34,17 @@ class ArrayStack:
         if self.is_empty():
             raise Empty('Stack is empty')
         return self._data.pop()
+    def transfer(self,s,t):
+        """Transfer Stack S into  empty T where S,last=t.top"""
+        for _ in range(len(S)):
+            t.push(s.pop())
+        
+l=[1,2,3,4,5]
+S=ArrayStack()
+
+for num in l:
+    S.push(num)
+print(S.top(),S._data, sep=('-> '))
+T=ArrayStack()
+T.transfer(S,T)
+print(T.top(),T._data,sep='-> ')
