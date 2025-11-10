c67a4e2c5e9261ab75ff14fcd35e8f62539a0e12
kamScripts <k.gebski1@gmail.com>
2025-11-10 17:47:12 +0000

    fisher-yates shuffle

--- week_3/week_8_sorting/data_generators.py
+++ week_3/week_8_sorting/data_generators.py
@@ -5,13 +5,20 @@ def get_random_list(n):
     i = 0
     arr=[None]*n
     for num in nums:
-        while i in taken_indices: 
+        while i in taken_indices:
             i = random.randint(1,n-1)
         arr[i]=num
         taken_indices.add(i)
     return arr
-
+def randomize_list(n):
+    """Randomize sequence of integers from 1 to n
+    using Fisher-Yates shuffle aLgorithm"""
+    arr=[num for num in range(1,n+1)]
+    for i in range(n-1,0,-1):
+        j=random.randint(0,i)
+        arr[i],arr[j]=arr[j],arr[i]
+    return arr
 if __name__=='__main__':
     
-    arr = get_random_list(10000)
+    arr = randomize_list(1000000)
     print(arr)
