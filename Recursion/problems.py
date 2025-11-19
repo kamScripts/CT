da5c1a6a2a170c670724509711dcab1d15257b56
kamScripts <k.gebski1@gmail.com>
2025-11-19 17:19:58 +0000

    find nth fibo number

--- Recursion/problems.py
+++ Recursion/problems.py
@@ -24,10 +24,20 @@ def sumList(lst: list[int | float]) -> int | float | None:
 def factorial(num:int)->int:
     """Find factorial of a number."""
     return num * factorial(num-1) if num >1 else num
+def find_n_fibo(num:int)->int:
+    """Find n-th number of Fibonacci Sequence"""
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return find_n_fibo(num-1) + find_n_fibo(num-2)
+   
 
 if __name__=='__main__':
 
     #print(reverse_string('software development'))
-    print(recursive_palindrome('racecar'))
-    print(sumList([1,2,3,4]), sumList([x for x in range(100)]), sep='\t')
-    print(factorial(5), 1*2*3*4*5)
+    #print(recursive_palindrome('racecar'))
+    #print(sumList([1,2,3,4]), sumList([x for x in range(100)]), sep='\t')
+    #print(factorial(5), 1*2*3*4*5)
+    print(find_n_fibo(50))
