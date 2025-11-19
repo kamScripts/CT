3118bba2946532291ad6f42919982034c38a056b
kamScripts <k.gebski1@gmail.com>
2025-11-19 16:27:58 +0000

    recursive palindrome checker

--- Recursion/problems.py
+++ Recursion/problems.py
@@ -5,6 +5,16 @@ def reverse_string(s:str)-> str:
     else:
         return s[-1] + reverse_string(s[:-1])
 
+def recursive_palindrome(s:str)->bool:
+    """Return True if string is palindrome."""
+    if len(s) == 1:
+        return True
+    if s[0] != s[-1]:
+        return False
+    return recursive_palindrome(s[1:-1])
+
+
 if __name__=='__main__':
 
-    print(reverse_string('software development'))
+    #print(reverse_string('software development'))
+    print(recursive_palindrome('racecar'))
